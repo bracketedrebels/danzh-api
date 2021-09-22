32764b72d62dfba04f1a30593bedc021020be9c7
wolfson@bracketedrebels.com <wolfson@bracketedrebels.com>
2021-09-22 13:58:57 +0300

    feat: trying to connect to redis from cloud

--- index.mjs
+++ index.mjs
@@ -1,13 +1,16 @@
 import express from "express"
-import schema from "./schema.mjs"
+import schemaFactory from "./schema.mjs"
 import { graphqlHTTP } from "express-graphql"
+import { createClient } from "redis"
 
 
-const { PORT, TARGET } = process.env
+const { PORT, TARGET, DB_HOST, DB_PORT, DB_PASSWORD } = process.env
 const graphiql = TARGET !== "production"
+const redisClient = createClient({host: DB_HOST, port: DB_PORT, no_ready_check: true, password: DB_PASSWORD })
+const schema = schemaFactory(redisClient);
 const gql = graphqlHTTP({ schema, graphiql })
-
 
 express()
   .use('/', gql)
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+ 
--- schema.mjs
+++ schema.mjs
@@ -1,19 +1,42 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 
-export default new GraphQLSchema({
+/**
+ * @type {(redisCleint: import("redis").RedisClient) => GraphQLSchema}
+ */
+export default (redisCleint) => new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
     fields: {
-      echo: {
+      get: {
         type: GraphQLString,
         args: {
-          str: {
+          key: {
             type: GraphQLString,
-            description: "Some string to echo",
-          }
+            description: "key to set",
+          },
         },
-        resolve: (_, { str }) => str
+        resolve: (_, { key }) => new Promise((r) => redisCleint.get(key, r))
       }
     }
+  }),
+  mutation: new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+      set: {
+        type: GraphQLString,
+        args: {
+          key: {
+            type: GraphQLString,
+            description: "key to set",
+          },
+          value: {
+            type: GraphQLString,
+            description: "value to set",
+          }
+        },
+        resolve: (_, { key, value }) => new Promise((r) => redisCleint.set(key, value, () => r(value)))
+      }
+    },
+
   })
 })
