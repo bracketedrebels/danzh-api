95b9c4895d1b7b2015467124b7a1d85af0036546
wolfson@bracketedrebels.com <wolfson@bracketedrebels.com>
2021-09-23 12:14:37 +0300

    fix: redis connection

--- index.mjs
+++ index.mjs
@@ -1,16 +1,37 @@
-import express from "express"
-import schemaFactory from "./schema.mjs"
-import { graphqlHTTP } from "express-graphql"
-import { createClient } from "redis"
+import express from "express";
+import schemaFactory from "./schema.mjs";
+import { graphqlHTTP } from "express-graphql";
+import R, { createClient } from "redis";
+import { tap } from "rambda";
+import { thunkify } from "ramda";
 
+const {
+  PORT = 5000,
+  TARGET = "development",
+  DB_HOST = "localhost",
+  DB_PORT = 13717,
+  DB_PASSWORD,
+  DB_USER,
+} = process.env;
+const graphiql = TARGET !== "production";
+const redis = createClient({
+  socket: {
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASSWORD,
+  },
+});
+const schema = schemaFactory(redis);
+const gql = graphqlHTTP({ schema, graphiql });
+const { info, error } = console;
 
-const { PORT, TARGET, DB_HOST, DB_PORT, DB_PASSWORD } = process.env
-const graphiql = TARGET !== "production"
-const redisClient = createClient({host: DB_HOST, port: DB_PORT, no_ready_check: true, password: DB_PASSWORD })
-const schema = schemaFactory(redisClient);
-const gql = graphqlHTTP({ schema, graphiql })
-
-express()
-  .use('/', gql)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
- 
+redis
+  .connect()
+  .then(tap(thunkify(info)("Connected to redis cloud")))
+  .then(() =>
+    express()
+      .use("/", gql)
+      .listen(PORT, thunkify(info)(`Listening on ${PORT}`))
+  )
+  .catch(error);
--- schema.mjs
+++ schema.mjs
@@ -1,42 +1,42 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 
 /**
- * @type {(redisCleint: import("redis").RedisClient) => GraphQLSchema}
+ * @type {(redisCleint: ReturnType<typeof import("redis").createClient>) => GraphQLSchema}
  */
-export default (redisCleint) => new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: {
-      get: {
-        type: GraphQLString,
-        args: {
-          key: {
-            type: GraphQLString,
-            description: "key to set",
+export default (redis) =>
+  new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: "Query",
+      fields: {
+        get: {
+          type: GraphQLString,
+          args: {
+            key: {
+              type: GraphQLString,
+              description: "key to set",
+            },
           },
+          resolve: (_, { key }) => redis.get(key),
         },
-        resolve: (_, { key }) => new Promise((r) => redisCleint.get(key, r))
-      }
-    }
-  }),
-  mutation: new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-      set: {
-        type: GraphQLString,
-        args: {
-          key: {
-            type: GraphQLString,
-            description: "key to set",
+      },
+    }),
+    mutation: new GraphQLObjectType({
+      name: "Mutation",
+      fields: {
+        set: {
+          type: GraphQLString,
+          args: {
+            key: {
+              type: GraphQLString,
+              description: "key to set",
+            },
+            value: {
+              type: GraphQLString,
+              description: "value to set",
+            },
           },
-          value: {
-            type: GraphQLString,
-            description: "value to set",
-          }
+          resolve: (_, { key, value }) => redis.set(key, value),
         },
-        resolve: (_, { key, value }) => new Promise((r) => redisCleint.set(key, value, () => r(value)))
-      }
-    },
-
-  })
-})
+      },
+    }),
+  });
